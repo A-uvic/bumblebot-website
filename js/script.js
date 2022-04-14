0235b4ccb877c65287372cfb7e0aa05f9e9a481a
Auvic <victor.trani1@gmail.com>
2022-04-13 22:12:01 -0300

    sticky navbar added

--- js/script.js
+++ js/script.js
@@ -2,7 +2,16 @@
 // Global Variables
 const container = document.getElementById("particle-container")
 const accordion = document.getElementsByClassName("acordion-item")
+
 var counter = 0
+
+//function to sticky navbar
+window.addEventListener("scroll", function (e) {
+    const navbar = document.getElementById("navbar");
+
+    navbar.classList.toggle("sticky", window.scrollY > 0)
+});
+
 
 
 // Function to generate aleatory number
@@ -14,9 +23,9 @@ const randomNumber = (min, max) => {
 const createParticle = () => {
     counter++
     const createParticle = document.createElement("div")
-    const sizeNumber = randomNumber(1.5,2)
+    const sizeNumber = randomNumber(1.5, 2)
 
-    
+
     createParticle.className = "particle"
     createParticle.style.cssText = `visibility: hidden; right: ${randomNumber(1, 98)}%; animation-duration: ${randomNumber(12, 25)}s; animation-delay: ${randomNumber(0.5, 1.5)}s; filter: blur(${randomNumber(0, 3)}px); width: ${sizeNumber}px; height: ${sizeNumber}px;`
     container.appendChild(createParticle)
@@ -32,10 +41,11 @@ const checkContainer = () => {
 //Event listeners for change classlist in FAQs container
 for (let i = 0; i < accordion.length; i++) {
 
-    accordion[i].addEventListener("click", function(){
+    accordion[i].addEventListener("click", function () {
         this.classList.toggle("active");
         this.childNodes[1].lastElementChild.classList.toggle("active")
     });
 }
+
 
 setInterval(checkContainer, 100);
