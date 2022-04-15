09c42008a658f1c3f9b961e16cf9681c393c0f80
Auvic <victor.trani1@gmail.com>
2022-04-14 21:57:21 -0300

    project updated

--- js/script.js
+++ js/script.js
@@ -2,17 +2,40 @@
 // Global Variables
 const container = document.getElementById("particle-container")
 const accordion = document.getElementsByClassName("acordion-item")
-
 var counter = 0
 
-//function to sticky navbar
+
+//Event listeners to sticky navbar
 window.addEventListener("scroll", function (e) {
     const navbar = document.getElementById("navbar");
 
     navbar.classList.toggle("sticky", window.scrollY > 0)
 });
 
+//Event listeners to reveal elements on screen
+window.addEventListener("scroll", function () {
+    var reveals = document.querySelectorAll(".reveal")
+    for (var i = 0; i < reveals.length; i++) {
+        var windowheight = window.innerHeight
+        var revealtop = reveals[i].getBoundingClientRect().top
+        var revealpoint = 150
 
+        if (revealtop < windowheight - revealpoint) {
+            reveals[i].classList.add("active")
+        } else {
+            reveals[i].classList.remove("active")
+        }
+    }
+})
+
+//Event listeners for change classlist in FAQs container
+for (let i = 0; i < accordion.length; i++) {
+
+    accordion[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        this.childNodes[1].lastElementChild.classList.toggle("active")
+    });
+}
 
 // Function to generate aleatory number
 const randomNumber = (min, max) => {
@@ -38,14 +61,8 @@ const checkContainer = () => {
     }
 }
 
-//Event listeners for change classlist in FAQs container
-for (let i = 0; i < accordion.length; i++) {
 
-    accordion[i].addEventListener("click", function () {
-        this.classList.toggle("active");
-        this.childNodes[1].lastElementChild.classList.toggle("active")
-    });
-}
+
 
 
 setInterval(checkContainer, 100);
